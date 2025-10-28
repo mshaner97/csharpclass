b056293f6b634b520df0aead75d51aeb6d400c3b
mshaner97 <mshaner97@icloud.com>
2025-10-28 13:06:29 -0500

    c sharp practice

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,25 +1,22 @@
-﻿//string[] fraudulentOrderIDs = new string[3];
+﻿Random dice = new Random();
 
-//fraudulentOrderIDs[0] = "A123";
-//fraudulentOrderIDs[1] = "B456";
-//fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-//string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-//Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-//Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-//Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-//fraudulentOrderIDs[0] = "F000";
-
-//Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-//Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-
-string[] fraudulentOrderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-foreach (string orders in fraudulentOrderIDs)
-if (orders.StartsWith("B"))
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
 {
-    Console.WriteLine(orders);
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20758a07eb2c5dad44aa3eab3dc0759f9f9a9365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4c1eb047677ac1cfc188f1922d5ba0e8c0ff4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
