ad0ea838aecaee7c618583b8bbf624bbdc8ef7d5
mshaner97 <mshaner97@icloud.com>
2026-04-26 11:47:49 -0500

    Csharp practice

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,32 +1,49 @@
-﻿using System.IO.Pipelines;
+﻿/*int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
 
-string[] orderStream = ["B123","C234","A345","C15","B177","G3003","C235","B179"];
-Array.Sort(orderStream);
-foreach (var orders in orderStream)
-{
-    if(orders.Length == 4)
-    {
-        Console.WriteLine($"{orders}");
-    }
-    else
-    {
-        Console.WriteLine($"{orders} - Error");
-    }
-}
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
 
-/*char[] orderArray = orderStream.ToCharArray();
-string result = 
-string[] items = result.Split(' ');
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
+
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine); */
+
+ string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName}, \nAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return. \n\nCurrently, you own {currentShares:N2} shares at a return of {currentReturn:P2}. \n\nOur new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
 
-Console.WriteLine(orderArray);
+Console.WriteLine("Here's a quick comparison:\n");
 
-/*string result = String.Join("",pangramArray);
-Console.WriteLine(result);
-string[] items = result.Split(' ');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
+// var formattedLine = currentProduct.PadRight(10);
+// string comparisonMessage = "";
+// Console.WriteLine(comparisonMessage);
+// Console.WriteLine($"{currentProduct}{currentReturn:P2}{currentProfit:C}");
+// Console.WriteLine($"{newProduct}{newReturn:P2}{newProfit:C}");
+ var comparisonMessage = currentProduct.PadRight(20);
+ comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+ comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(10);
 
-Console.WriteLine(result);*/
+ Console.WriteLine(comparisonMessage);
